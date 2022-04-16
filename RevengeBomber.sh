3444b701032c4074134b5b08fd27134078ec953b
Alex Bieber <94044755+alexbieber@users.noreply.github.com>
2022-04-16 17:17:56 +0530

    Add files via upload

---
+++ RevengeBomber.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+
+detect_distro() {
+    if [[ "$OSTYPE" == linux-android* ]]; then
+            distro="termux"
+    fi
+
+    if [ -z "$distro" ]; then
+        distro=$(ls /etc | awk 'match($0, "(.+?)[-_](?:release|version)", groups) {if(groups[1] != "os") {print groups[1]}}')
+    fi
+
+    if [ -z "$distro" ]; then
+        if [ -f "/etc/os-release" ]; then
+            distro="$(source /etc/os-release && echo $ID)"
+        elif [ "$OSTYPE" == "darwin" ]; then
+            distro="darwin"
+        else 
+            distro="invalid"
+        fi
+    fi
+}
+
+pause() {
+    read -n1 -r -p "Press any key to continue..." key
+}
+banner() {
+    clear
+    echo -e "\e[1;33m"
+    if ! [ -x "$(command -v figlet)" ]; then
+        echo 'Introducing RevengeBomber'
+    else
+        figlet RevengeBomber
+    fi
+    if ! [ -x "$(command -v toilet)" ]; then
+        echo -e "\e[4;37m This Bomber Was Created By \e[1;32mAlexBieber \e[0m"
+    else
+        echo -e "\e[1;34mCreated By \e[1;37m"
+        toilet -f mono12 -F border AlexBieber
+    fi
+    echo -e "\e[1;31m For Any Queries Join Me!!!\e[0m"
+    echo -e "\e[1;35m           Telegram: https://t.me/RevengeBomber \e[0m"
+    echo -e "\e[4;36m   YouTube: https://www.youtube.com/channel/UCwXEnG4Ne6q6LJhjaGgm3UA \e[0m"
+    echo " "
+    echo "NOTE: Kindly move to the PIP version Of RevengeBomb for more stability."
+    echo " "
+}
+
+init_environ(){
+    declare -A backends; backends=(
+        ["arch"]="pacman -S --noconfirm"
+        ["debian"]="apt-get -y install"
+        ["ubuntu"]="apt -y install"
+        ["termux"]="apt -y install"
+        ["fedora"]="yum -y install"
+        ["redhat"]="yum -y install"
+        ["SuSE"]="zypper -n install"
+        ["sles"]="zypper -n install"
+        ["darwin"]="brew install"
+        ["alpine"]="apk add"
+    )
+
+    INSTALL="${backends[$distro]}"
+
+    if [ "$distro" == "termux" ]; then
+        PYTHON="python"
+        SUDO=""
+    else
+        PYTHON="python3"
+        SUDO="sudo"
+    fi
+    PIP="$PYTHON -m pip"
+}
+
+install_deps(){
+    
+    packages=(openssl git $PYTHON $PYTHON-pip figlet toilet)
+    if [ -n "$INSTALL" ];then
+        for package in ${packages[@]}; do
+            $SUDO $INSTALL $package
+        done
+        $PIP install -r requirements.txt
+    else
+        echo "We could not install dependencies."
+        echo "Please make sure you have git, python3, pip3 and requirements installed."
+        echo "Then you can execute revengeRevengeBomber.py ."
+        exit
+    fi
+}
+
+banner
+pause
+detect_distro
+init_environ
+if [ -f .update ];then
+    echo "All Requirements Found...."
+else
+    echo 'Installing Requirements....'
+    echo .
+    echo .
+    install_deps
+    echo This Script Was Made By AlexBieber > .update
+    echo 'Requirements Installed....'
+    pause
+fi
+while :
+do
+    banner
+    echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
+    echo " "
+    echo "Press 1 To  Start SMS  Bomber "
+    echo "Press 2 To  Start CALL Bomber "
+    echo "Press 3 To  Start MAIL Bomber (Not Yet Available)"
+    echo "Press 4 To  Update (Works On Linux And Linux Emulators) "
+    echo "Press 5 To  Exit "
+    read ch
+    clear
+    if [ $ch -eq 1 ];then
+        $PYTHON RevengeBomber.py --sms
+        exit
+    elif [ $ch -eq 2 ];then
+        $PYTHON RevengeBomber.py --call
+        exit
+    elif [ $ch -eq 3 ];then
+        $PYTHON RevengeBomber.py --mail
+        exit
+    elif [ $ch -eq 4 ];then
+        echo -e "\e[1;34m Downloading Latest Files..."
+        rm -f .update
+        $PYTHON RevengeBomber.py --update
+        echo -e "\e[1;34m RUN RevengeBomber Again..."
+        pause
+        exit
+    elif [ $ch -eq 5 ];then
+        banner
+        exit
+    else
+        echo -e "\e[4;32m Invalid Input !!! \e[0m"
+        pause
+    fi
+done
